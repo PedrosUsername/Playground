87af978925c8a5ece2813d3ab9367c773684f692
rogs <pilique11@gmail.com>
2021-10-02 04:40:42 -0300

    update looknfeel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClockComponent } from './clock/clock.component';
+import { StopwatchComponent } from './stopwatch/stopwatch.component';
+import { TimerComponent } from './timer/timer.component';
+import { TimerContainerComponent } from './timer2/timer-container/timer-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'clock', component: ClockComponent },
+  { path: 'stopwatch', component: StopwatchComponent },
+  { path: 'timer', component: TimerComponent },
+  { path: 'timer-container', component: TimerContainerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'playground';
+  state_widget = 0
+
+
+  constructor(
+    private router: Router
+  ){}
+
+
+  _where(state: number): string {
+    switch ( state ) {
+      case 1:
+        return '/stopwatch'
+        break;
+      case 2:
+        return '/timer'
+        break;
+      case 3:
+        return '/timer-container'
+        break;          
+
+      default: 
+        return '/clock'
+        break;
+    }    
+  }
+
+  arrowL(): void {
+    if(this.state_widget > 0)
+      this.state_widget--
+    else
+      this.state_widget = 3
+
+    this.router.navigate([ this._where(this.state_widget) ])
+  }
+
+  arrowR(): void {
+    if(this.state_widget < 3)
+      this.state_widget++
+    else
+      this.state_widget = 0
+
+    this.router.navigate([ this._where(this.state_widget) ])
+  }
 }
